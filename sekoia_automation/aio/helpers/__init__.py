64561b4126c9708d43a04e12b4ce5c28b68156ba
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2024-02-23 17:46:13 +0100

    fix(AIO): use semaphore instead of creating batch of tasks

--- sekoia_automation/aio/helpers/__init__.py
+++ sekoia_automation/aio/helpers/__init__.py
@@ -5,22 +5,22 @@ NOTE!!!: each package inside requires additional libraries to be installed.
 """
 
 import asyncio
-import itertools
 from typing import AsyncGenerator, Any
 
 
-async def limit_concurrency(aws: list, limit: int) -> AsyncGenerator[Any, None]:
+async def limit_concurrency(tasks: list, limit: int) -> AsyncGenerator[Any, None]:
     """
     Limit the number of concurrent async tasks at the same time
 
-    :param list aws: The list of async tasks to execute
+    :param list tasks: The list of async tasks to execute
     :param: int limit: The number of concurrent async tasks allows at the same time
 
     """
-    aws = iter(aws)
-    while True:
-        batch = list(itertools.islice(aws, limit))
-        if not batch:
-            break
-        for result in await asyncio.gather(*batch):
-            yield result
+    semaphore = asyncio.Semaphore(limit)
+
+    async def sem_coro(task):
+        async with semaphore:
+            return await task
+
+    for result in await asyncio.gather(*map(sem_coro, tasks)):
+        yield result

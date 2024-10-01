6a6187a2877dfeaaca32b3ee56cb8d3febe7828c
Alexandre Léonardi <alexandre.leonardi@sekoia.io>
2024-10-01 17:34:56 +0200

    Add account validation module
    Added an AccountValidator class that can be registered when instaciating
    a module in the same way as triggers or actions
    This class handles the account validation process, calling an abstract
    validator method that the user will have to fill in himself, and sending
    the return value of validator to a Symphony callback endpoint

---
+++ tests/test_account_validator.py
@@ -0,0 +1,80 @@
+from unittest.mock import call, patch
+
+import pytest
+import requests
+import requests_mock
+
+from sekoia_automation.account_validator import AccountValidator
+
+
+class TestAccountValidator(AccountValidator):
+    def validator(self) -> bool:
+        return True
+
+
+def test_execute_success():
+    validator = TestAccountValidator()
+
+    with (
+        patch.object(
+            validator.module, "load_config", return_value="http://example.com/callback"
+        ) as mock_load_config,
+        requests_mock.Mocker() as mock_request,
+    ):
+        mock_request.post("http://example.com/callback", status_code=200)
+
+        validator.execute()
+
+        assert mock_load_config.call_args_list == [
+            call(validator.VALIDATION_CALLBACK_URL_FILE_NAME),
+            call(validator.TOKEN_FILE_NAME),
+        ]
+        assert mock_request.called
+        assert mock_request.last_request.json() == {"validation_status": True}
+
+
+def test_execute_failure():
+    class FailingAccountValidator(AccountValidator):
+        def validator(self) -> bool:
+            return False
+
+    validator = FailingAccountValidator()
+
+    with (
+        patch.object(
+            validator.module, "load_config", return_value="http://example.com/callback"
+        ) as mock_load_config,
+        requests_mock.Mocker() as mock_request,
+    ):
+        mock_request.post("http://example.com/callback", status_code=200)
+
+        validator.execute()
+
+        assert mock_load_config.call_args_list == [
+            call(validator.VALIDATION_CALLBACK_URL_FILE_NAME),
+            call(validator.TOKEN_FILE_NAME),
+        ]
+        assert mock_request.called
+        assert mock_request.last_request.json() == {"validation_status": False}
+
+
+def test_execute_request_failure():
+    validator = TestAccountValidator()
+
+    with (
+        patch.object(
+            validator.module, "load_config", return_value="http://example.com/callback"
+        ) as mock_load_config,
+        requests_mock.Mocker() as mock_request,
+    ):
+        mock_request.post("http://example.com/callback", status_code=500)
+
+        with pytest.raises(requests.exceptions.HTTPError):
+            validator.execute()
+
+        assert mock_load_config.call_args_list == [
+            call(validator.VALIDATION_CALLBACK_URL_FILE_NAME),
+            call(validator.TOKEN_FILE_NAME),
+        ]
+        assert mock_request.called
+        assert mock_request.last_request.json() == {"validation_status": True}

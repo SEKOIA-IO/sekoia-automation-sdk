090e08b62ea0c839248ef1ecda55926b93b938b5
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-08-19 17:29:29 +0200

    Add software fields

--- sekoia_automation/asset_connector/models/ocsf/software.py
+++ sekoia_automation/asset_connector/models/ocsf/software.py
@@ -1,5 +1,36 @@
-from pydantic import BaseModel
+from sekoia_automation.asset_connector.models.ocsf.base import OCSFBaseModel
+from sekoia_automation.asset_connector.models.ocsf.device import Device
 
 
-class SoftwareOCSFModel(BaseModel):
-    pass
+class SoftwarePackage:
+    name:str
+    version:str
+    uid:str | None = None
+    cpe_name:str | None = None
+    license:str | None = None
+    license_url:str | None = None
+    release:str | None = None
+    type:str | None = None
+    type_id:int | None = None
+
+
+class SoftwareComponent:
+    version:str
+    name:str
+    author:str | None = None
+    type:str | None = None
+    type_id:int | None = None
+
+
+class SoftwareBillOfMaterials:
+    package: SoftwarePackage
+    software_components: list[SoftwareComponent] | None = None
+    type: str | None = None
+    type_id:int | None = None
+    uid: str | None = None
+    version: str | None = None
+
+
+class SoftwareOCSFModel(OCSFBaseModel):
+    device: Device
+    sbom: SoftwareBillOfMaterials | None = None

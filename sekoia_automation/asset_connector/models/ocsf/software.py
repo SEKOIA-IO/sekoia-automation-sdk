93b610ba298c65293d7d2bf326cc13eb9bf7f887
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-08-27 11:37:55 +0200

    chore: update ocsf fields

--- sekoia_automation/asset_connector/models/ocsf/software.py
+++ sekoia_automation/asset_connector/models/ocsf/software.py
@@ -1,7 +1,55 @@
+from enum import IntEnum, Enum
+
 from pydantic import BaseModel
 
 from sekoia_automation.asset_connector.models.ocsf.base import OCSFBaseModel
 from sekoia_automation.asset_connector.models.ocsf.device import Device
+
+
+class SBOMTypeId(IntEnum):
+    UNKNOWN = 0
+    SPDX = 1
+    CYCLOEDX = 2
+    SWID = 3
+    OTHER = 99
+
+
+class SBOMTypeStr(Enum):
+    UNKNOWN = "Unknown"
+    SPDX = "SPDX"
+    CYCLOEDX = "CycloDX"
+    SWID = "SWID"
+    OTHER = "Other"
+
+
+class ComponentTypeId(IntEnum):
+    UNKNOWN = 0
+    FRAMEWORK = 1
+    LIBRARY= 2
+    OPERATINGSYSTEM= 3
+    OTHER = 99
+
+
+class ComponentTypeStr(Enum):
+    UNKNOWN = "Unknown"
+    FRAMEWORK = "Framework"
+    LIBRARY = "Library"
+    OPERATINGSYSTEM = "Operating System"
+    OTHER = "Other"
+
+
+class PackageTypeId(IntEnum):
+    UNKNOWN = 0
+    APPLICATION = 1
+    OPERATINGSYSTEM= 2
+    OTHER = 99
+
+
+class PackageTypeStr(Enum):
+    UNKNOWN = "Unknown"
+    APPLICATION = "Application"
+    OPERATINGSYSTEM = "Operating System"
+    OTHER = "Other"
 
 
 class SoftwarePackage(BaseModel):
@@ -12,23 +60,23 @@ class SoftwarePackage(BaseModel):
     license: str | None = None
     license_url: str | None = None
     release: str | None = None
-    type: str | None = None
-    type_id: int | None = None
+    type: PackageTypeStr | None = None
+    type_id: PackageTypeId | None = None
 
 
 class SoftwareComponent(BaseModel):
     version: str
     name: str
     author: str | None = None
-    type: str | None = None
-    type_id: int | None = None
+    type: ComponentTypeStr | None = None
+    type_id: ComponentTypeId | None = None
 
 
 class SoftwareBillOfMaterials(BaseModel):
     package: SoftwarePackage
     software_components: list[SoftwareComponent] | None = None
-    type: str | None = None
-    type_id: int | None = None
+    type: SBOMTypeStr | None = None
+    type_id: SBOMTypeId | None = None
     uid: str | None = None
     version: str | None = None
 

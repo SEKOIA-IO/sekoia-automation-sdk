f6475bfa53102b5ecadf79ec487f5107c428d896
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2025-09-22 23:48:07 +0200

    feat(SDK): add configuration for fission

--- sekoia_automation/configuration/__init__.py
+++ sekoia_automation/configuration/__init__.py
@@ -1,7 +1,9 @@
+import os
 from functools import lru_cache
 
 from sekoia_automation.configuration.base import Configuration
 from sekoia_automation.configuration.filesystem import FileSystemConfiguration
+from sekoia_automation.configuration.fission import FissionConfiguration
 
 
 @lru_cache
@@ -9,4 +11,8 @@ def get_configuration() -> Configuration:
     """
     Get the configuration instance to use in the module.
     """
-    return FileSystemConfiguration()
+    runtime = os.environ.get("SYMPHONY_RUNTIME")
+    if runtime is not None and runtime.lower() == "fission":
+        return FissionConfiguration()
+    else:
+        return FileSystemConfiguration()

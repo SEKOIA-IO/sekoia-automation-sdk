32bf431c58ad4c38c4bd24fb1cc1fb7c429d03bd
Sebastien Quioc <sebastien.quioc@sekoia.fr>
2026-04-13 13:47:28 +0200

    fix(SDK): remove duplicated BaseModel config

--- tests/test_action.py
+++ tests/test_action.py
@@ -464,7 +464,6 @@ def test_action_with_arguments_model():
     class TestActionArguments(SekoiaAutomationBaseModel):
         field: str = "value"
         number: int = 0
-        model_config = ConfigDict(coerce_numbers_to_str=True)
 
     class TestAction(Action):
         def run(self, arguments: TestActionArguments):
